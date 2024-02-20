907562cb196743e31f6012a44f68c729ce0215b5
Alex <zarjrw1994@gmail.com>
2024-02-20 16:22:34 +0300

    Added task 3.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using BSUIR_Lab_8_Tasks_2_3;
+using System.Collections;
+
+//Task 2
 
 User alex = new User("Alex", "Petrov", 24, 'F');
 User max = new User("Maxim", "Sidorov", 22, 'F');
@@ -15,4 +18,25 @@ users.Push(alex);
 
 UserStackConsoleMenu<User> menu = new UserStackConsoleMenu<User>(users);
 menu.Start();
+
+//Task 3
+//Создайте обобщенный метод, который получает массив произвольного типа и возвращает количество элементов, не равных null.
+
+User[] users1 = new User[] { alex, max, olga, null, vika, null, null, anna };
+object[] users2 = new User[] { alex, max, olga, null, vika, null, null };
+List<User> users3 = new List<User>() {olga, null, vika, null, null };
+
+Console.WriteLine($"В массиве users1 {CountNotNull<User>(users1)} не null элементов!");
+Console.WriteLine($"В массиве users2 типа object {CountNotNull<object>(users2)} не null элементов!");
+Console.WriteLine($"В списке users3 {CountNotNull<User>(users3.ToArray())} не null элементов!");
+
+int CountNotNull<T>(T[] arr)
+{
+    int counter = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] is not null) counter++;
+    }
+    return counter;
+}
 
